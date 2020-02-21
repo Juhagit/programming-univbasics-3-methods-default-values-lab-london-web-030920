2c7a61deeb83cfe43512ff631931920be11a17b9
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-21 02:48:30 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,6 @@
 def meal_choice(protein = "meat" veg1, veg2)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
   
-  meal_choice("broccoli", "macaroni")
-
 end 
+
+meal_choice("broccoli", "macaroni")
